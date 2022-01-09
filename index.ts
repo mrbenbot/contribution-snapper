869a62591e33c4bfb7ebdde11a77899b982cf0db
Ben Lee <benlee.thatsme@gmail.com>
2022-01-09 20:04:40 +0000

    remove nodemailer and uplad as artifact

--- index.ts
+++ index.ts
@@ -1,41 +1,14 @@
 import puppeteer from "puppeteer";
-import nodemailer from "nodemailer";
-import Mail from "nodemailer/lib/mailer";
 
-const image = await getImage();
-await sendEmail(image);
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
 
-async function getImage(): Promise<Buffer> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://github.com/mrbenbot", {
-    waitUntil: "networkidle2",
-  });
-  await page.waitForSelector(".js-calendar-graph");
+await page.goto("https://github.com/mrbenbot", {
+  waitUntil: "networkidle2",
+});
+await page.waitForSelector(".js-calendar-graph");
 
-  const element = await page.$(".js-calendar-graph");
-  const image = await element?.screenshot({ quality: 100, type: "jpeg" });
+const element = await page.$(".js-calendar-graph");
+await element?.screenshot({ path: "./image.jpeg", quality: 100 });
 
-  await browser.close();
-
-  return image as Buffer;
-}
-
-async function sendEmail(image: Buffer) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.FROM_EMAIL,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
-
-  const mailOptions: Mail.Options = {
-    from: process.env.FROM_EMAIL,
-    to: process.env.TO_EMAIL,
-    subject: `GitHub Contribution Image (${new Date().toDateString()})`,
-    attachments: [{ filename: "image.jpg", content: image }],
-  };
-
-  await transporter.sendMail(mailOptions);
-}
+await browser.close();
