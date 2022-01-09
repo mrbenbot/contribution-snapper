7430e55ed541d895d0554e23538ddf1653f983cd
Ben Lee <benlee.thatsme@gmail.com>
2022-01-09 20:16:11 +0000

    set device scale factor to 2

--- index.ts
+++ index.ts
@@ -7,8 +7,12 @@ await page.goto("https://github.com/mrbenbot", {
   waitUntil: "networkidle2",
 });
 await page.waitForSelector(".js-calendar-graph");
+page.setViewport({ width: 1648, height: 1400, deviceScaleFactor: 2 });
 
 const element = await page.$(".js-calendar-graph");
-await element?.screenshot({ path: "./image.jpeg", quality: 100 });
+await element?.screenshot({
+  path: "./image.jpeg",
+  quality: 100,
+});
 
 await browser.close();
